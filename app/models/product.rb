3ba991adfa99bfaa8671fbfa31d4704fd456ae4c
Robinpreet Kaur Malhi <rmalhi2@academic.rrc.ca>
2024-03-20 15:54:33 -0500

    added more products to the database

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
 
+  
+
   scope :search_by_keyword_and_category, ->(keyword, category_id) do
     if category_id.present?
       where("product_name LIKE ? OR product_description LIKE ?", "%#{keyword}%", "%#{keyword}%").where(category_id: category_id)
