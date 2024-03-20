86c80e15e76ee759c0d42ba367923877f76bbcae
Robinpreet Kaur Malhi <rmalhi2@academic.rrc.ca>
2024-03-19 23:24:50 -0500

    created Product index and show page to display the products

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'products/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :products, only: [:index, :show]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
