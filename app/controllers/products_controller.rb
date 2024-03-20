edec7ef810cd0e7332dd9a248ba6fa772984dfe4
Robinpreet Kaur Malhi <rmalhi2@academic.rrc.ca>
2024-03-20 01:28:06 -0500

    added search functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.page(params[:page]).per(10) # Display 10 products per page
+    @products = Product.search_by_keyword_and_category(params[:keyword], params[:category_id]).page(params[:page]).per(10) # Display 10 products per page
   end
 
   def show
