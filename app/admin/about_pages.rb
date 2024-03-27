0d59f492c4587cd1b2f45a0e7105788f1c931843
Robinpreet Kaur Malhi <rmalhi2@academic.rrc.ca>
2024-03-27 02:19:31 -0500

    generated active admin resources for about page and contact page

---
+++ app/admin/about_pages.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register AboutPage do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :paragraph1, :paragraph2, :paragraph3, :paragraph4, :paragraph5
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :paragraph1, :paragraph2, :paragraph3, :paragraph4, :paragraph5]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/contact_pages.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register ContactPage do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :phone, :email, :address
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :phone, :email, :address]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
